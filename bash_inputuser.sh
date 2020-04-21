c31abf237cde79c2d24a81f6ec6eb7485c85b7a0
Pro <i.pogorelko@gmail.com>
2020-04-21 19:15:17 +0300

    transform to function and simple validation of data

--- bash_inputuser.sh
+++ bash_inputuser.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+function inputparameters()
+{
+
 # enter User
 read -p "Please enter Username: " NEWUSER
 read -p "Please enter Password: " PASSWORD
 
-# trim spaces and special symbols
+}
 
+inputparameters
+
+# validation of data
+flagValidationParameters=0
+
+while [[ $flagValidationParameters != 1 ]]; do
+
+	if [ -z "${NEWUSER}" ] || [ -z "${PASSWORD}" ]; then
+		echo "One of required options is empty. Please Try again"
+		inputparameters
+	else
+		flagValidationParameters=1
+        
+	fi
+done
+
+# trim spaces and symbols
 NEWUSER=${NEWUSER//[^[:alnum:]\n\r]/}
 PASSWORD=${PASSWORD//[^[:alnum:]\n\r]/}
 
